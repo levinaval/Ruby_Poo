4d8f0f1082343d0edf8e3b6f2629382893a13d30
levi <levisantosluna@gmail.com>
2021-07-13 21:17:55 -0300

    re-provado, recuperaçao, aprovado

--- atividades_poo/algoritmo_16.rb
+++ atividades_poo/algoritmo_16.rb
@@ -1,1 +1,3 @@
 #Escreva um algoritmo que receba o nome de um aluno e 3 notas e faça a média aritmética. E mostre se o aluno foi aprovado se a média for maior ou igual a 6.0, recuperação se for menor que 6 e maior ou igual a 3, e se reprovado se a nota for menor que 3. O algoritmo deve reconhecer e negar operação quando a nota for menor que zero ou maior que 10
+#Entrada: 6.8, 9.5, 3.9
+#Saída: Aluno: Fulano, Média: 6,73 - Aprovado
