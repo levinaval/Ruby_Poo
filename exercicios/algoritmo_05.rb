28f9402ba2f34278a1ab89f6d9aab547b8fe4795
levi <levisantosluna@gmail.com>
2021-07-13 21:01:48 -0300

    multiplicados por 5 Exiba os valores dos dois vetores na tela, simultaneamente

--- exercicios/algoritmo_05.rb
+++ exercicios/algoritmo_05.rb
@@ -1,5 +1,5 @@
 #Crie um programa que solicite a entrada de 10 números pelo usuário, armazenando-os em um vetor, e então monte outro vetor, com os valores do primeiro multiplicados por 5.
-#Exiba os valores dos dois vetores na tela, simultaneamente, em duas colunas (um em cada coluna), uma posição por linha.
+##Exiba os valores dos dois vetores na tela, simultaneamente, em duas colunas (um em cada coluna), uma posição por linha.
 
 lista1 = []
 lista2 = []
