cb643a08b0c09fdbf412ac01140e8967f8571a03
levi <levisantosluna@gmail.com>
2021-07-13 21:02:53 -0300

    verifique se começa com uma vogal ou consoante

--- exercicios/algoritmo_06.rb
+++ exercicios/algoritmo_06.rb
@@ -1,4 +1,4 @@
-#Crie um programa que solicite um nome e verifique se começa com uma vogal ou consoante.
+##Crie um programa que solicite um nome e verifique se começa com uma vogal ou consoante.
 def vogal_consoante(palavra)
   if  palavra[0] == "A" || palavra[0] == "E" || palavra[0] == "I" || palavra[0] == "O" || palavra[0] == "U"
     puts "o nome coneça com vogal !!!"
