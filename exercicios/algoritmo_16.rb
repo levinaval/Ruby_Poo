6ee314d46953c7e1df7f9c7a08321271fa779d5a
levi <levisantosluna@gmail.com>
2021-07-19 21:13:33 -0300

    numeros pares, impares, positivo e negativo

--- exercicios/algoritmo_16.rb
+++ exercicios/algoritmo_16.rb
@@ -1,1 +1,4 @@
 #Faça um algoritmo que leia um número inteiro e mostre uma mensagem indicando se este número é par ou ímpar, e se é positivo ou negativo.
+
+puts "escreva um numero."
+numero = gets.chomp.to_i
