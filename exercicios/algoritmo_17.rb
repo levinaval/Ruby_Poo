3d4f9068c27a596a150ef7080de6c24344b72a4d
levi <levisantosluna@gmail.com>
2021-07-29 21:26:47 -0300

    incerir dados

--- exercicios/algoritmo_17.rb
+++ exercicios/algoritmo_17.rb
@@ -4,4 +4,7 @@ notas = []
 puts "1. adiciona aluno e nota, 2. sair:"  
    cod = gets.chomp.to_i
    if cod == 1
-    
+    puts "Digite o nome do aluno."
+     aluno = gets.chomp
+     nota1 = gets.chomp.to_f
+     
