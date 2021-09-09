25c3d31977089cdd34bdc0a2203bb2a07b237098
levi <levisantosluna@gmail.com>
2021-09-08 23:26:19 -0300

    inicio

--- exercicios/agenda.rb
+++ exercicios/agenda.rb
@@ -2,6 +2,17 @@
 #com nome e numero
 #podendo ver todos os contatos ou somente um.
 
+@agenda = [
+    {nome: "Fulano", telefone "90000-0000"}
+    {nome: "flamengo", telefone "91111-1111"}
+]
+
+def todos_os_contatos
+    @agenda.each do /contato/
+      puts "#{contato[:nome]} - #{:telefone}"
+    end
+end
+
 loop do
   
   puts "1-Contatos, 2-Adicionar, 3-Ver contatos, 4-Editar contato, 5-Remover, 0-Sair"
@@ -12,9 +23,6 @@ loop do
   when codigo == 0
     puts "Sair... Até mais"
     break
-   
-    
-  
-    
+
   end
 end
