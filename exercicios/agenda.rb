dcd8fe3b54c915edfce7da2960d69470f714bba9
levi <levisantosluna@gmail.com>
2021-09-08 23:15:32 -0300

    inicio

--- exercicios/agenda.rb
+++ exercicios/agenda.rb
@@ -5,4 +5,16 @@
 loop do
   
   puts "1-Contatos, 2-Adicionar, 3-Ver contatos, 4-Editar contato, 5-Remover, 0-Sair"
+
+  codigo = gets.chomp.to_i
+
+  case 
+  when codigo == 0
+    puts "Sair... Até mais"
+    break
+   
+    
+  
+    
+  end
 end
