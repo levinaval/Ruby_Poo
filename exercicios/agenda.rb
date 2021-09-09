b12b2fa879f042e03e6c409b62e3fe128f94e0e0
levi <levisantosluna@gmail.com>
2021-09-08 23:11:10 -0300

    inicio

--- exercicios/agenda.rb
+++ exercicios/agenda.rb
@@ -1,3 +1,8 @@
 #adicionar, editar e remover
 #com nome e numero
 #podendo ver todos os contatos ou somente um.
+
+loop do
+  
+  puts "1-Contatos, 2-Adicionar, 3-Ver contatos, 4-Editar contato, 5-Remover, 0-Sair"
+end
