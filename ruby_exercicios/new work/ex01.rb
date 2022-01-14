68c0cf326bb00dfc9fd900e6ac418c8da30f518e
levi <levisantosluna@gmail.com>
2022-01-13 23:07:45 -0300

    new beginning

--- ruby_exercicios/new work/ex01.rb
+++ ruby_exercicios/new work/ex01.rb
@@ -1,8 +1,8 @@
 #puts "hello world!!" /O que estiver entre aspas duplas é uma String/
 
-nome = "Levi" /isto é uma variavel/
+nome = "Levi" #isto é uma variavel
 idade = 37
 
-puts "hello #{nome}" /Deforma não precisamos efetuar mudanças em todas as linhas. só na variavel!/
+puts "hello #{nome}" #Deforma não precisamos efetuar mudanças em todas as linhas. só na variavel!
 
 puts "#{nome} tem #{idade} e esta estudando para se tornar um Dv"
