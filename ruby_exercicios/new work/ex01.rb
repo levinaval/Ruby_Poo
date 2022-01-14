4e24c2e775b0a0ca99695392c1f82aeb0e84af51
levi <levisantosluna@gmail.com>
2022-01-14 19:39:05 -0300

    String e variavel

--- ruby_exercicios/new work/ex01.rb
+++ ruby_exercicios/new work/ex01.rb
@@ -1,8 +1,3 @@
-#puts "hello world!!" /O que estiver entre aspas duplas é uma String/
-
-#nome = "Levi" #isto é uma variavel(Quarda informações a serem apresentadas.)
-#idade = 37
-
 puts "Me diga seu nome."
 nome = gets.chomp
 
@@ -11,6 +6,15 @@ idade = gets.chomp.to_i
 
 puts "-----------------------------"
 
-puts "hello #{nome}!!" #Deforma não precisamos efetuar mudanças em todas as linhas. só na variavel!
+puts "hello #{nome}!!" 
 
 puts "Você tem #{idade} e esta estudando para se tornar um Dv"
+
+#" " /O que estiver entre aspas duplas é uma String/
+
+#nome = " " isto é uma variavel(Quarda informações a serem apresentadas.)
+#idade =
+
+# Devemos utilizar #{ } para aparecer a informação da de uma Variavel. desta forma não precisamos efetuar mudanças em todas as linhas. só na variavel!
+
+# A função do "CHOMP" é de abstrair o mometo em que se aperta o enter.
