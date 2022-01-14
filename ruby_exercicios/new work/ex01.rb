fa314f01063ce3be5bd30fe11ec6014d9aaf4b15
levi <levisantosluna@gmail.com>
2022-01-14 19:21:25 -0300

    new beginning

--- ruby_exercicios/new work/ex01.rb
+++ ruby_exercicios/new work/ex01.rb
@@ -1,8 +1,10 @@
 #puts "hello world!!" /O que estiver entre aspas duplas é uma String/
 
-nome = "Levi" #isto é uma variavel(Quarda informações a serem apresentadas.)
-idade = 37
+#nome = "Levi" #isto é uma variavel(Quarda informações a serem apresentadas.)
+#idade = 37
 
+puts "Me diga seu nome."
+nome = get.chomp
 puts "hello #{nome}" #Deforma não precisamos efetuar mudanças em todas as linhas. só na variavel!
 
 puts "#{nome} tem #{idade} e esta estudando para se tornar um Dv"
