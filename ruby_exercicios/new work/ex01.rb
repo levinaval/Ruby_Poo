e9ad14aca38e3c339221cf9b96ff7a8424b899c0
levi <levisantosluna@gmail.com>
2022-01-14 19:25:32 -0300

    new beginning

--- ruby_exercicios/new work/ex01.rb
+++ ruby_exercicios/new work/ex01.rb
@@ -4,13 +4,13 @@
 #idade = 37
 
 puts "Me diga seu nome."
-nome = get.chomp
+nome = gets.chomp
 
 puts "Agora quero saber sua idade."
-idade = get.chomp.to_i
+idade = gets.chomp.to_i
 
 puts "-----------------------------"
 
-puts "hello #{nome}" #Deforma não precisamos efetuar mudanças em todas as linhas. só na variavel!
+puts "hello #{nome}!!" #Deforma não precisamos efetuar mudanças em todas as linhas. só na variavel!
 
-puts "#{nome} tem #{idade} e esta estudando para se tornar um Dv"
+puts "Você tem #{idade} e esta estudando para se tornar um Dv"
