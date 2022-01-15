5b39b8ee643333ef8b0862acafb2e7b40928afbf
levi <levisantosluna@gmail.com>
2022-01-14 21:07:14 -0300

    Numeros e Operaçoes Matematicas

--- ruby_exercicios/new work/ex02.rb
+++ ruby_exercicios/new work/ex02.rb
@@ -2,8 +2,27 @@ puts "Vamos calcular? Sim ou Não?"
 decisao = gets.chomp
 
 if decisao == "sim"
-puts "Escolha a operação"
+puts "Escolha entre uma dessas operações"
 
 elsif 
   puts "Ok... Ate a proxina!"
+end
+
+puts "Soma", "Subtração", "Multiplicação", "Divisão"
+puts "---------------------"
+escolha = gets.chomp
+
+soma = x + y
+subtracao = x - y
+multiplicacao = x * y
+divisao = x.to_f / y.to_f
+
+if escolha == "soma"
+  
+  puts "Digite o primeiro número"
+  x = gets.chomp.to_i
+  puts "Digite o segundo número"
+  y = gets.chomp.to_i
+  puts soma
+
 end
