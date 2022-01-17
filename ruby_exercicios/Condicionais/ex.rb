20a968892daf01c391c7f358365c9b090dfe838a
levi <levisantosluna@gmail.com>
2022-01-16 21:58:22 -0300

    Condicional

---
+++ ruby_exercicios/Condicionais/ex.rb
@@ -0,0 +1,14 @@
+puts "Qual é a sua profição?"
+
+nome = gets.chomp
+
+if nome == "motorista"
+  puts "profição em extinção!"
+  
+elsif nome == "progamador"
+  puts "Grande profição em crescimento!!"
+  
+else
+  puts "Ainda tem algun futuro! kkk"
+  
+end
