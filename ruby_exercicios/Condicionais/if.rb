1636be1b173e8e3d4724b5935ed8ad7516668b00
levi <levisantosluna@gmail.com>
2022-01-16 21:44:29 -0300

    Condicional

--- ruby_exercicios/Condicionais/if.rb
+++ ruby_exercicios/Condicionais/if.rb
@@ -3,6 +3,13 @@ x = 10
 if x == 11
   puts "X é igual a 10"
 
+  elsif x == 50
+    puts "É igual a 50"
+
  else
   puts "X não é igual a 10"
 end
+
+# if- É uma comparação.(se for iqual...)
+# elsif- É uma comparação.(é outra coisa!)
+# else- É uma comparação(se não for isso é aquilo)
