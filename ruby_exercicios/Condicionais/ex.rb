464e08e0a9cf802f413cc1dee18c046de176300b
levi <levisantosluna@gmail.com>
2022-01-16 21:32:31 -0300

    Condicional

--- ruby_exercicios/Condicionais/ex.rb
+++ ruby_exercicios/Condicionais/ex.rb
@@ -1,11 +1,20 @@
-and 
-Verdade e Verdade = Verdade
-Verdade e Falso = Falso
-Falso e Falso = Falso
-Falso e Verdade = Falso
+#and 
+#Verdade e Verdade = Verdade
+#Verdade e Falso = Falso
+#Falso e Falso = Falso
+#Falso e Verdade = Falso
 
-or
-Verdade ou Verdade = Verdade
-Verdade ou Falso = Falso
-Falso ou Falso = Falso
-Falso ou Verdade =Verdade
+#or
+#Verdade ou Verdade = Verdade
+#Verdade ou Falso = Falso
+#Falso ou Falso = Falso
+#Falso ou Verdade =Verdade
+
+#not 
+#Não Verdade = Falso
+#Não Falso = Verdade
+#...
+
+puts false && true
+puts false || true
+puts !false && true
