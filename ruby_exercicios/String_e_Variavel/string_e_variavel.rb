6c4b505f82681403ed26628326e9cf601fb85c6e
levi <levisantosluna@gmail.com>
2022-01-17 22:32:14 -0300

    String e Variavel

---
+++ ruby_exercicios/String_e_Variavel/string_e_variavel.rb
@@ -0,0 +1,14 @@
+puts "Hello world!!"
+puts
+
+puts "Me diga seu nome."
+nome = gets.chomp
+
+puts "Agora quero saber sua idade."
+idade = gets.chomp.to_i
+
+puts "-----------------------------"
+
+puts "hello #{nome}!!" 
+
+puts "Você tem #{idade} e esta estudando para se tornar um Dv"
