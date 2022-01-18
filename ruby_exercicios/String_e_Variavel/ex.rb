094032ca12046a3132589ea6a82d7df6fa50b41d
levi <levisantosluna@gmail.com>
2022-01-17 22:55:19 -0300

    String e Variavel

--- ruby_exercicios/String_e_Variavel/ex.rb
+++ ruby_exercicios/String_e_Variavel/ex.rb
@@ -4,3 +4,9 @@ rua = gets.chomp
 puts "--------------------"
 
 puts "A rua #{rua}, é muito tranquila!!"
+puts "..."
+puts "Qual o número da sua casa?"
+
+numero = gets.chomp.to_i
+puts "---------------------"
+puts "So a sua casa que tem número #{numero} nesta rua?"
