915bc8c050c9d58f36be094b393ab6109887c259
levi <levisantosluna@gmail.com>
2022-01-25 20:09:54 -0300

    teste

--- ruby_exercicios/teste.rb
+++ ruby_exercicios/teste.rb
@@ -5,8 +5,15 @@ resposta = gets.chomp
 if resposta == "sim"
   puts "Qual o mês em que voçê nasceu?"
 
+elsif resposta == "não"
+  puts "Paramos por aque!!"
+
 else
-  "Então paramos por aque!"
+  puts "Não foi isso que perguntei."
+  loop do
+    break #corrigir
+
+  end
 end
 
 mes = gets.chomp
@@ -21,12 +28,20 @@ when "março"
   puts "Tem 31 dias"
 when "abril"
   puts "tem 30 dias"
-  when "maio"
+when "maio"
   puts "tem 31 dias"
 when "junho"
   puts "tem 30 dias"
-when "maio"
+when "julho"
   puts "tem 31 dias"
-else
-  puts "Isso eu não sei!!"
+when "agosto"
+  puts "tem 31 dias"
+when "setenbro"
+  puts "tem 30 dias"
+when "outubro"
+  puts "tem 31 dias"
+when "novembro"
+  puts "tem 30 dias"
+when "dezembro"
+  puts "tem 31 dias"
 end
