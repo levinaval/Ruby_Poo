2ea9de9d2a5e267e83ac71a580a281c5f2b80337
levi <levisantosluna@gmail.com>
2022-01-24 22:20:24 -0300

    ex

--- ruby_exercicios/arrays, vetores ou lista/ex.rb
+++ ruby_exercicios/arrays, vetores ou lista/ex.rb
@@ -1,7 +1,15 @@
 #lista = [ 3, 5, 6, 1, "string", [11, 56, 89]]
 #puts lista
 
+#lista = []
+
+#lista.push("ha", "he", "hi")
+#puts lista[0, 2] 
+
 lista = []
 
-lista.push("ha", "he", "hi")
-puts lista[0, 2] 
+lista.push("Feijão", "Farinha", "Macarrão")
+lista << "Arroz"
+
+lista.insert(2, "cuscuz")
+puts lista
