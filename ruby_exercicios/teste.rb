2b1238c26b5cac97c6ae27dc57091a264d53bb66
levi <levisantosluna@gmail.com>
2022-01-25 19:52:34 -0300

    teste

--- ruby_exercicios/teste.rb
+++ ruby_exercicios/teste.rb
@@ -1,4 +1,4 @@
-puts "Vamos interagir?"
+puts "Vamos interagir, sim ou não?"
 
 resposta = gets.chomp
 
@@ -10,3 +10,23 @@ else
 end
 
 mes = gets.chomp
+
+case mes
+
+when "janeiro"
+  puts "Tem 31 dias"
+when "fevereiro"
+  puts "Tem 28 dias"
+when "março"
+  puts "Tem 31 dias"
+when "abril"
+  puts "tem 30 dias"
+  when "maio"
+  puts "tem 31 dias"
+when "junho"
+  puts "tem 30 dias"
+when "maio"
+  puts "tem 31 dias"
+else
+  puts "Isso eu não sei!!"
+end
