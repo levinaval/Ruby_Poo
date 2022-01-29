4301a26b999a743f7684e7a700864b6f82c3ae3d
levi <levisantosluna@gmail.com>
2022-01-29 20:07:45 -0300

    repetiçao

--- ruby_exercicios/08.repetições/for.rb
+++ ruby_exercicios/08.repetições/for.rb
@@ -1,7 +1,11 @@
 nomes = ["Colher", "Martelo", "Prego", "Parafuso"]
 
-construçao = {material:"Cimento", quantidade:5, local:5.5}
+construcao = {material:"Cimento", quantidade:5, local:5.5}
 
-for nome in nomes do
-  puts nomes
+#for nome in nomes do
+#  puts nomes
+#end
+
+for k, v in construcao do
+  puts "#{k}: #{v}"
 end
