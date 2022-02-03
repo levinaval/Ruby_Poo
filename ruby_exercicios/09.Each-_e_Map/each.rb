1fc0cc794112f6b7d183e44f6a185d2ec4f17192
levi <levisantosluna@gmail.com>
2022-02-02 22:08:32 -0300

    Each e Map

--- ruby_exercicios/09.Each-_e_Map/each.rb
+++ ruby_exercicios/09.Each-_e_Map/each.rb
@@ -2,6 +2,10 @@ cesto = ["Laranja", "Maçã", "Peira", "uva"]
 
 dict = {produtor:"roberto", idade:56, peso:85.60}
 
-cesto.each do |cesto|
- puts cesto
+#cesto.each do |cesto|
+# puts cesto
+#end
+
+dict.each do |chave, valor|
+  puts "#{chave}: #{valor}"
 end
