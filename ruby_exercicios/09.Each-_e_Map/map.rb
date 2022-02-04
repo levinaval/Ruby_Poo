f2f92ebbc026374fffd338762d2b031a6c90441e
levi <levisantosluna@gmail.com>
2022-02-03 22:07:54 -0300

    Each e Map

--- ruby_exercicios/09.Each-_e_Map/map.rb
+++ ruby_exercicios/09.Each-_e_Map/map.rb
@@ -1,15 +1,15 @@
 nome = ["Pedro", "Marcos", "Pereira", "monica"]
 
-nomes_completos = nomes.map do |nome_completos|
-  nome_completos + " sobrenome"
-end
+#nomes_completos = nomes.map do |nome_completos|
+#  nome_completos + " sobrenome"
+#end
 
-puts nomes
-puts "----------"
-puts nomes_completos
+#puts nomes
+#puts "----------"
+#puts nomes_completos
 
 nome.map! do |nome_completo|
   nome_completo + " sobrenome"
 end
 
-puts nomes
+puts nome
