ef4b675f37b2e831778d694a8473e3ddc54c8a2e
levi <levisantosluna@gmail.com>
2022-03-09 22:43:32 -0300

    array, vetores ou lista

--- ruby_exercicios/05.arrays, vetores ou lista/organisar.rb
+++ ruby_exercicios/05.arrays, vetores ou lista/organisar.rb
@@ -5,8 +5,9 @@ lista.push("ho", "hu", "ha", "he", "hi")
 
 lista_organizada = lista.sort
 puts lista_organizada
+# obs: Para organizar em ordem alfabetica ou em primeiro, segundo...
 
-# obs:  
+# obs: 
 puts lista.sort.first
         or 
 puts lista.sort.last
