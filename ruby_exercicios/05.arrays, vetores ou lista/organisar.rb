93a7c357755e5d54446b2dbe6b6fa4ce2ba1abef
levi <levisantosluna@gmail.com>
2022-03-09 22:46:23 -0300

    array, vetores ou lista

--- ruby_exercicios/05.arrays, vetores ou lista/organisar.rb
+++ ruby_exercicios/05.arrays, vetores ou lista/organisar.rb
@@ -7,7 +7,7 @@ lista_organizada = lista.sort
 puts lista_organizada
 # obs: Para organizar em ordem alfabetica ou em primeiro, segundo...
 
-# obs: 
+# obs: Para chamar só o primeiro ou o último.
 puts lista.sort.first
         or 
 puts lista.sort.last
