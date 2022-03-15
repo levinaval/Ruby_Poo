f558562d07d59f70da57dd1aaef2f38b0c9bb864
levi <levisantosluna@gmail.com>
2022-03-15 19:47:31 -0300

    array, vetores ou lista

--- ruby_exercicios/05.arrays, vetores ou lista/texte.rb
+++ ruby_exercicios/05.arrays, vetores ou lista/texte.rb
@@ -1,20 +1,25 @@
-puts "Olá! vamos trabalhar com lista?"
-decisao = gets.chomp
-
-if decisao == "sim"
-  puts "Incira dois nomes."
-
-elsif == "não"
-  puts "Xau!!"
-
-else
-  "Não é isso que eu quero saber."
-end
-
-lista = []
+lista = ["Sopa", "Cahorro Quente", "Amburgue", "Batata Frita", "Filer Parmagiana", "Pastel"]
 
 lista.push()
 
 lista_organizada = lista.sort
 
-puts lista_organizada
+puts "Olá! benvindo a lanchonete"
+puts "Quer fazer o seu pedido?"
+decisao = gets.chomp
+
+if decisao == "sim"
+  puts "Este é o menu."
+  puts
+  puts lista_organizada
+
+elsif decisao == "nao"
+  puts "Xau!!"
+
+else
+  "Não é isso que eu quero saber."
+
+end
+lista = []
+lista = gets.chomp
+
