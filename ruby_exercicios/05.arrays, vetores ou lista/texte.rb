9d171fa574f3b7b3cdb176d799ba5cd1e39727e4
levi <levisantosluna@gmail.com>
2022-03-17 22:43:20 -0300

    array, vetores ou lista

--- ruby_exercicios/05.arrays, vetores ou lista/texte.rb
+++ ruby_exercicios/05.arrays, vetores ou lista/texte.rb
@@ -1,141 +1,125 @@
-menu_de_lanche = ["Sopa", "Cahorro Quente", "Amburgue", "Batata Frita", "Filer Parmagiana", "Pastel"]
 
+menu_de_lanche = ["Sopa", "Cachorro Quente", "Amburgue", "Batata Frita", "Filer Parmagiana", "Pastel"]
+ 
 menu_de_lanche.push()
-
+ 
 menu_de_lanche_organizada = menu_de_lanche.sort
-  puts "Olá! benvindo a lanchonete do Levi"
-  puts
-  puts "Gostaria de fazer o seu pedido?"
-  puts
-
-  decisao = gets.chomp
-
+ puts "Olá! benvindo a lanchonete do Levi"
+ puts
+ puts "Gostaria de fazer o seu pedido?"
+ puts
+ 
+ decisao = gets.chomp
+ 
 if decisao == "sim"
-  puts
-  puts "Este é o menu."
-  puts 
-  puts "---------------"
-  puts menu_de_lanche_organizada
-  puts "-----------------"
-
-
- elsif decisao == "nao"
-  puts
-  puts "Certo, espero que volte logo. xau!!"
-
- else
-  puts
-  puts "Não é isso que eu quero saber."
-
-end
-
-menu_de_lanche = []
-
-menu_de_lanche << gets.chomp
-
-menu_de_bebidas = ["Suco", "Refrigerante", "Água de cocô", "Caldo de cana"]
-
-menu_de_bebidas.push()
-
-menu_de_bebidas_organisada = menu_de_bebidas.sort
-
-  puts
-  puts "-----------"
-  puts "Algo para beber, Sim ou Não?"
-  puts
-
-decisao = gets.chomp
-
-if decisao == "sim"
-  puts
-  puts "Menu de bebidas"
-  puts "---------------"
-  puts menu_de_bebidas
-  puts
-  puts "---------------"
-
+ puts
+ puts "Este é o menu."
+ puts
+ puts "---------------"
+ puts menu_de_lanche_organizada
+ puts "-----------------"
+ 
+ 
 elsif decisao == "nao"
-  puts "Certo!"
-    
+ puts
+ puts "Certo, espero que volte logo. xau!!"
+ 
 else
-  puts "Não entende..."  
+ puts
+ puts "Não é isso que eu quero saber."
+ 
 end
-
+ 
+menu_de_lanche = []
+ 
+menu_de_lanche << gets.chomp
+ 
+menu_de_bebidas = ["Suco", "Refrigerante", "Água de cocô", "Caldo de cana"]
+ 
+menu_de_bebidas.push()
+ 
+menu_de_bebidas_organisada = menu_de_bebidas.sort
+ 
+ puts
+ puts "-----------"
+ puts "Algo para beber, Sim ou Não?"
+ puts
+ 
+decisao = gets.chomp
+ 
+if decisao == "sim"
+ puts
+ puts "Menu de bebidas"
+ puts "---------------"
+ puts menu_de_bebidas
+ puts
+ puts "---------------"
+ 
+elsif decisao == "nao"
+ puts "Certo!"
+  
+else
+ puts "Não entende..." 
+end
+ 
 menu_de_bebidas = []
-
+ 
 menu_de_bebidas << gets.chomp
-
-  puts
-  puts "--------------"
-  puts "Ok..."
-  puts
-  puts "Confirmando o seu pedidio"
-  puts "------------------"
-  puts
-  puts "Lanche:"
-  puts menu_de_lanche
-  puts "-----------------"
-  puts
-  puts "Bebida:"
-  puts menu_de_bebidas
-  puts
-  puts "Deseja acrecentar mais algum pedido?"
-  puts
-
-acrecentar = gets.chomp
-
-if acrecentar == "sim"
-  puts
-  puts "Acrecentando pedido!"
-  puts
-
- elsif acrecentar == "nao"
-  puts
-  puts "Finalisando o pedido..."
-  puts
-  puts "Aperte ENTER para conferir seu padido."
-  puts
-
- else
-  puts "Opçâo não reconhecida!"
-
+ 
+ puts
+ puts "--------------"
+ puts "Ok..."
+ puts
+ puts "Confirmando o seu pedido"
+ puts "------------------"
+ puts
+ puts "Lanche:"
+ puts menu_de_lanche
+ puts "-----------------"
+ puts
+ puts "Bebida:"
+ puts menu_de_bebidas
+ puts
+ puts "Deseja acrescentar mais algum pedido?"
+ puts
+ 
+acrescentar = gets.chomp
+ 
+if acrescentar == "sim"
+ puts
+ puts "Acrescentando pedido!"
+ puts
+ 
+elsif acrescentar == "nao"
+ puts
+ puts "Finalizando o pedido..."
+ puts
+ puts "Aperte ENTER para conferir seu pedido."
+ puts
+ 
+else
+ puts "Opção não reconhecida!"
+ 
 end
-
-menu_pedido_acrecentado = gets.chomp
-
-#if escolha == "lanche"
-#  puts "Acrecente o que dezeja."
-
-#  elsif escolha == "bebida"
-#  puts "Acrecente o que dezeja."
-
- # else
-  #puts "Opção não reconhecida!"
-
-#end
+ 
+menu_pedido_acrescentado = gets.chomp
 
 pedido_acrecentado = []
-
-#pedido_acrecentado.push()
-
-#menu_pedido_acrecentado = pedido_acrecentado.sort
-
-pedido_acrecentado << gets.chomp
-
-  
-
-  puts "-----------"
-  puts "Pedidio finalizado!"
-  puts "------------------"
-  puts "Lanche:"
-  puts menu_de_lanche
-  puts "-----------------"
-  puts
-  puts "Bebida:"
-  puts menu_de_bebidas
-  puts
-  puts "-----------------"
-  puts
-  puts "Pedido acrecentado:"
-  puts menu_pedido_acrecentado
-  
+ 
+pedido_acrescentado << gets.chomp
+ 
+ 
+ puts "-----------"
+ puts "Pedido finalizado!"
+ puts "------------------"
+ puts "Lanche:"
+ puts menu_de_lanche
+ puts "-----------------"
+ puts
+ puts "Bebida:"
+ puts menu_de_bebidas
+ puts
+ puts "-----------------"
+ puts
+ puts "Pedido acrescentado:"
+ puts menu_pedido_acrescentado
