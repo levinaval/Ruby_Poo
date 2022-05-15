24b0e3bc610251e4e562fd50fc576a5d0f043a86
levi <levisantosluna@gmail.com>
2022-05-14 21:14:36 -0300

    definiçao do else

--- ruby_exercicios/03.Condicionais/introducao.rb
+++ ruby_exercicios/03.Condicionais/introducao.rb
@@ -20,5 +20,8 @@ puts false || true
 puts !false && true
 
 if 
-Expressão que verifiva se uma condição é verdadeora(true), e a partir deste resultado determina se as instruções dentro de seu corpo serão ou não executadas.
+  Expressão que verifiva se uma condição é verdadeora(true), e a partir deste resultado determina se as instruções dentro de seu corpo serão ou não executadas.
   
+else
+  Informa o que fazer quando a verificaçãode uma condição IF for falada.
+
