65cc8caa726b663f3f69a239ee800828197e373c
levi <levisantosluna@gmail.com>
2022-05-14 21:02:01 -0300

    refatoraçao do arquivo nome e idade

--- ruby_exercicios/01.String_e_Variavel/nome_e_idade.rb
+++ ruby_exercicios/01.String_e_Variavel/nome_e_idade.rb
@@ -4,14 +4,14 @@ first_name = gets.chomp
 puts "________________"
 print "Em que ano estamos?"
 
-y = gets.chomp.to_i
+current_year = gets.chomp.to_i
 puts "________________"
 
 print "Ok... Por favor agora me informe o ano em que você nasceu:"
 
-x = gets.chomp.to_i
+year_of_birth = gets.chomp.to_i
 puts "________________"
 
-subt = y - x 
+subt = current_year - year_of_birth 
 
 puts "Seja muito bem vindo #{first_name}, você tem apenas #{subt} e pode crescer e apender muita coisa ainda!"
