f9c792b841755d5f7f85990bebc2aaf4eb61e25f
levi <levisantosluna@gmail.com>
2022-05-14 21:51:30 -0300

    if not

--- ruby_exercicios/03.Condicionais/unless.rb
+++ ruby_exercicios/03.Condicionais/unless.rb
@@ -8,4 +8,16 @@ end
 
 puts "A loja esta #{loja}"
 
-#Inverte a comparasão.. 
+#Inverte a comparasão..
+
+loja = "fechada"
+
+if not loja == "aberta"
+  loja = "Aberta.. pode entra!"
+else
+  loja = "Fechada.. não pode entra"
+end
+
+puts "A loja esta #{loja}"
+
+#Tem o mesmo efeito!
