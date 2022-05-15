7602a872b3a1ebaf71efe8dda89497bc6bbc3b6d
levi <levisantosluna@gmail.com>
2022-05-14 21:39:46 -0300

    outro tipo de conparaçao

--- ruby_exercicios/03.Condicionais/ex.rb
+++ ruby_exercicios/03.Condicionais/ex.rb
@@ -1,14 +1,23 @@
-puts "Qual é a sua profição?"
+#puts "Qual é a sua profição?"
+#
+#nome = gets.chomp
+#
+#if nome == "motorista"
+#  puts "profição em extinção!"
+#  
+#elsif nome == "progamador"
+#  puts "Grande profição em crescimento!!"
+#  
+#else
+#  puts "Ainda tem algun futuro! kkk"
+#  
+#end
 
-nome = gets.chomp
+carro = "versa"
+marca = "deixa para la"
 
-if nome == "motorista"
-  puts "profição em extinção!"
-  
-elsif nome == "progamador"
-  puts "Grande profição em crescimento!!"
-  
-else
-  puts "Ainda tem algun futuro! kkk"
-  
+if carro != "versa"
+  marca = "nissa"
 end
+
+puts "qual é seu carro? #{marca}!"
