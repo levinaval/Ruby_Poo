5fc311cd3ccec937297303135d26e8baf4a67964
levi <levisantosluna@gmail.com>
2022-05-22 17:41:10 -0300

    comentarios

--- ruby_exercicios/05.arrays, vetores ou lista/acessar.rb
+++ ruby_exercicios/05.arrays, vetores ou lista/acessar.rb
@@ -6,3 +6,8 @@ lista.push("ha", "he", "hi", "kk", "rr")
 print lista[0..4] 
 
 ##código para acessar os ultimos item em uma variável.
+
+lista = []
+
+lista.push("ha", "he", "hi", "kk", "rr")
+puts lista[-3..-1]
