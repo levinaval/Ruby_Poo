c95b11baa066062968fa795dcda93b579fbb1f7b
levi <levisantosluna@gmail.com>
2022-05-23 09:29:27 -0300

    respondendo primeiro exercio

---
+++ atividades_em_ruby/IFRN/primeiros_passos/01.questao.rb
@@ -0,0 +1,10 @@
+#Calcule a soma dos números de 1 a 10, atribua a variável x e mostre seu valor.
+
+c = 1 
+
+10.times do
+
+  puts c
+  c += 1
+  
+end
---
+++ atividades_em_ruby/IFRN/primeiros_passos/02.rb
@@ -0,0 +1,14 @@
+# (b²-4*a*c)
+
+def Equação_do_segundo_grau
+
+  (b²)(-4*a)(*c) = d
+  
+end
+
+a = 2
+b = 5
+c = 2
+d = 9
+
+puts Equação_do_segundo_grau
