b32e05e37de0aa8255dd1412712ffa899a5e5874
levi <levisantosluna@gmail.com>
2022-05-31 20:18:09 -0300

    each

---
+++ ruby_exercicios/09.Each_e_Map/each_hash2.rb
@@ -0,0 +1,8 @@
+salas = {"Sala 1 " => "Liberda", "Sala 2 " => "Liberda", "Sala 3 " => "Em limpesa"}
+
+salas.each do |cadeado, porta|# Liberdade para colocar o nome de sua preferencia para ser o argumento e chamar "chave" e "valor"
+
+  puts "#{cadeado} #{porta}"
+
+end
+
