03520207c56608803cf2b59b8f657f1f0678d3ab
levi <levisantosluna@gmail.com>
2022-06-01 19:05:51 -0300

    array.map

--- ruby_exercicios/09.Each_e_Map/map2.rb
+++ ruby_exercicios/09.Each_e_Map/map2.rb
@@ -1,20 +1,20 @@
 array = [1, 2, 3, 4]
 
-new_array = array.map do |a|
-
-  a * 4
-
-end
-
-puts "Resultado dos calculos!"
-puts "Primeiro #{array}"
-puts
-puts "Segundo #{new_array}"
-
-
-#array.map! do |a|
+#new_array = array.map do |a|
 #
-#  a * 2
+#  a * 4
 #
 #end
-#puts "#{new_array}"
+#
+#puts "Resultado dos calculos!"
+#puts "Primeiro #{array}"
+#puts
+#puts "Segundo #{new_array}"
+
+
+array.map! do |a|
+
+  a * 2
+
+end
+puts " #{array}"
