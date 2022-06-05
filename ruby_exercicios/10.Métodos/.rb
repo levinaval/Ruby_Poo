47fa01fc9d93f8c11700d4c367d4532ab0712ec3
levi <levisantosluna@gmail.com>
2022-06-05 16:52:24 -0300

    treinando métodos

---
+++ ruby_exercicios/10.Métodos/.rb
@@ -0,0 +1,10 @@
+def lista(estado="vazio")
+  
+ puts "A lista esta #{estado}."
+
+end
+
+lista
+
+estado = "cheio"
+lista(estado)
--- ruby_exercicios/10.Métodos/signal.rb
+++ ruby_exercicios/10.Métodos/signal.rb
@@ -8,3 +8,14 @@ signal
 
 color = "white"
 signal(color)
+
+#def lista(estado="vazio")
+#  
+#  puts "A lista esta #{estado}."
+# 
+# end
+# 
+# lista
+# 
+# estado = "cheio"
+# lista(estado)
