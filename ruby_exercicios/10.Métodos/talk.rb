b90e7e5577f159094401affff68d844f0c3073fa
levi <levisantosluna@gmail.com>
2022-06-04 21:41:04 -0300

    treinando

--- ruby_exercicios/10.Métodos/talk.rb
+++ ruby_exercicios/10.Métodos/talk.rb
@@ -6,3 +6,15 @@ first_name = "Levi"
 last_name = "Silva"
 
 talk(first_name, last_name)
+
+
+#def curso(radiologia, enfermagem)
+#
+#  puts "   A técnica de radiologia #{radiologia},trabalha com certa dose de radiação. Já a técnica em enfermagem #{enfermagem}, trabalha limpando a sujeira dos pacientes."
+#
+#end
+#
+#radiologia = "Louise Mendonça"
+#enfermagem = "Laura Miler"
+#
+#curso(radiologia, enfermagem)
