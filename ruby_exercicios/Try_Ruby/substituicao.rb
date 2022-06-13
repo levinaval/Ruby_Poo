fd699f27f53a377b3311ed2f06984b1612425b0a
levi <levisantosluna@gmail.com>
2022-06-13 09:39:31 -0300

    organisando os arquivos

---
+++ ruby_exercicios/Try_Ruby/substituicao.rb
@@ -0,0 +1,21 @@
+#O método gsub é abreviação para substituição global. Ele substitui todas as ocorrências de uma arrey para outra arrey.
+
+def poema poema
+
+  puts poema
+  
+  end
+  
+  poema "Minha manga voou da minha mão
+    E minha manga se foi para a lua.
+    Mas quando eu vejo isso na televisão,
+    Colocando nossa bandeira no cometa Halley,
+    Mais ainda eu quero come-la." 
+    
+   poema "Minha torrada voou da minha mão
+    E minha torrada se foi para a lua.
+    Mas quando eu vejo isso na televisão,
+    Colocando nossa bandeira no cometa Halley,
+    Mais ainda eu quero come-la."
+  
+  #puts poema.gsub!("torrada", "manga")
