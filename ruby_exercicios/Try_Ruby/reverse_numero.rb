5aa8ddb5d95776db1af02257b318d563c9f7b728
levi <levisantosluna@gmail.com>
2022-06-13 09:37:50 -0300

    organisando os arquivos

---
+++ ruby_exercicios/Try_Ruby/reverse_numero.rb
@@ -0,0 +1,15 @@
+# Para reverter um número é preciso transformalo em um String
+
+lista = []
+
+lista = gets.chomp.to_s
+
+puts lista.reverse
+
+#concatenação
+
+s = "Levi"
+
+s << "Silva"
+
+puts s 
