ffcf904d4a09f9267d1695df75f07b218928821e
levi <levisantosluna@gmail.com>
2022-06-13 15:41:02 -0300

    class computer

---
+++ Abstração/computador.rb
@@ -0,0 +1,12 @@
+class Computer
+  def turning_on
+    "Liguegando o computador!"
+  end
+
+  def turning_off
+   "Desligando o computador!"    
+  end
+end
+
+computer = Computer.new
+puts computer.turning_off
