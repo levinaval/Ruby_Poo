a9eb762265249ccf63bb9400d1cc4ee94f9994a9
levi <levisantosluna@gmail.com>
2022-06-19 13:58:15 -0300

    Boletim Poo

--- Abstração/boletim.rb
+++ Abstração/boletim.rb
@@ -8,15 +8,21 @@ puts "notas."
 nota1 = gets.chomp.to_f
 nota2 = gets.chomp.to_f
 nota3 = gets.chomp.to_f
+
+def boletim(aluno, nota1, nota2, nota3)
+  
 soma = nota1 + nota2 + nota3 
 media = soma / 3
 
-if media >= 6 && media <= 10
-  puts "aluno: #{aluno} media: #{media} - Aprovado"
- elsif media < 6 && media >= 3 
-  puts "aluno: #{aluno} media: #{media} - Recuperação!"
- elsif media < 3 && media >= 0
-  puts"aluno: #{aluno} media: #{media} - Reprovado"
- else
-   puts "Alguma nota está inválida"
+ if media >= 6 && media <= 10
+   puts "aluno: #{aluno} media: #{media} - Aprovado"
+  elsif media < 6 && media >= 3 
+   puts "aluno: #{aluno} media: #{media} - Recuperação!"
+  elsif media < 3 && media >= 0
+   puts"aluno: #{aluno} media: #{media} - Reprovado"
+  else
+    puts "Alguma nota está inválida"
  end
+end
+
+boletim(aluno, nota1, nota2, nota3)
---
+++ Abstração/fraction.rb
@@ -0,0 +1,13 @@
+# Crie uma classe chamada Fraction para representar uma fração matemática.
+#Ela deve ter os seguintes métodos:
+
+# Fraction#initalize - Construtor para receber o numerador e denominador
+
+# Fraction#to_f - Método para conversão para Float
+
+# Fraction#to_s - Método que retorna um string com a fração
+
+# Fraction#* - Método que recebe outro objeto de Fração ou número inteiro e retorna uma novo
+#objeto do tipo Fraction com o resultado da multiplicação
+
+#Métodos de acesso às variáveis de numerador e denominador
