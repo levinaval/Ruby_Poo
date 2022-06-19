29c8e8052b490096838fa01ed3ab207f3c2e1d10
levi <levisantosluna@gmail.com>
2022-06-19 18:59:50 -0300

    adicionando novos elementos a local Poo

--- escopo_de_variaves/local.rb
+++ escopo_de_variaves/local.rb
@@ -1,5 +1,5 @@
 # Declarada com a primeira letra de seu nome com uma letra minúscula ou sublinhado.
-# Só pode ser acessada apenas onde foi criada. 
+# Só pode ser acessada apenas onde foi criada, dentro do método, classe, Etc. 
 
 def casa
   local = "Esta casa é na rua Clovis Cavalcante."
@@ -7,3 +7,7 @@ def casa
 end
 
 casa
+
+# vareavel de mesmo nome, porem com outro valor.
+casa = 1287
+puts casa
