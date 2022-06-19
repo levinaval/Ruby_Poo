c9d17346fc34e67b5acde96b21cd471af5432ac6
levi <levisantosluna@gmail.com>
2022-06-19 19:12:37 -0300

    global Poo

--- escopo_de_variaves/global.rb
+++ escopo_de_variaves/global.rb
@@ -1,2 +1,25 @@
 #Declarada com o prefixo $
 # Pode ser acessada em qualquer lugar do programa. OBS: "Fortemente desencorajado o seu uso."
+
+class Restalrante
+  def primeiro_pedido
+    $global = 0
+    puts $global
+  end
+end
+
+class Lanchonete
+  def segundo_pedido
+    $global += 1
+    puts $global    
+  end
+end
+
+restalrante = Restalrante.new
+lanchonete = Lanchonete.new
+
+restalrante.primeiro_pedido
+lanchonete.segundo_pedido
+lanchonete.segundo_pedido
+# Por isso é muito perigosa! pode ser chamada por qualquer método, qualquer lugar pode altera-la. 
+puts $global
