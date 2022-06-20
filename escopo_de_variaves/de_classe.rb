305e9ed1b0ab1ea0ce1df323e4c9d12bc8027683
levi <levisantosluna@gmail.com>
2022-06-19 21:03:41 -0300

    de instacia Poo

--- escopo_de_variaves/de_classe.rb
+++ escopo_de_variaves/de_classe.rb
@@ -1,2 +1,17 @@
 # Declarada com o prefixo @@.
 # Pode ser acessada em qualquer lugar da classe onde foi declarada e seu valor é compartilhado entre todas as instâncias de sua classe.
+
+class User
+@@user_count = 0
+ def add(nome)
+  puts "User #{nome} adicionado."
+  @@user_count += 1
+  puts @@user_count 
+ end  
+end
+
+first_user = User.new
+first_user.add("Levi")
+
+second_user = User.new
+second_user.add("Luna")
--- escopo_de_variaves/de_instacia.rb
+++ escopo_de_variaves/de_instacia.rb
@@ -1,2 +1,17 @@
 # Seu nome começa com o símbolo @.
 # Semelhante a variável de classe, tendo como única diferença o valor que não é compartilhado entre todas as instância de sua classe.
+
+class Usuario
+ def add(name)
+   @name = name
+   puts "Usuario adicionado"
+   hello
+ end  
+
+ def hello
+   puts "Seja bem vindo, #{@name}!"
+ end
+end
+
+user = Usuario.new
+user.add("Levi")
