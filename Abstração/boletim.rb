07ea3ace8cc4449244ed886bd65acfc4ad78e09c
levi <levisantosluna@gmail.com>
2022-06-20 19:36:26 -0300

    exercício compras Poo

--- Abstração/boletim.rb
+++ Abstração/boletim.rb
@@ -2,32 +2,36 @@
 #Entrada: 6.8, 9.5, 3.9
 #Saída: Aluno: Fulano, Média: 6,73 - Aprovado
 
+class School
 
-
-class Escola
-  
-  puts "insira o nome do aluno e as notas."
+  puts "insira o nome do aluno."
   aluno = gets.chomp
   puts "notas."
   nota1 = gets.chomp.to_f
   nota2 = gets.chomp.to_f
   nota3 = gets.chomp.to_f
 
- def boletim(aluno, nota1, nota2, nota3)
- soma = nota1 + nota2 + nota3 
- media = soma / 3
-  if media >= 6 && media <= 10
-    puts "aluno: #{aluno} media: #{media} - Aprovado"
-   elsif media < 6 && media >= 3 
-    puts "aluno: #{aluno} media: #{media} - Recuperação!"
-   elsif media < 3 && media >= 0
-    puts"aluno: #{aluno} media: #{media} - Reprovado"
-   else
-     puts "Alguma nota está inválida"
+  def initialize(aluno, nota1, nota2, nota3)
+    @aluno = aluno
+    @nota1 = nota1
+    @nota2 = nota2
+    @nota3 = nota3
   end
- end
+
+  def boletim(aluno, nota1, nota2, nota3)
+   soma = nota1 + nota2 + nota3 
+   media = soma / 3
+   if media >= 6 && media <= 10
+     puts "aluno: #{aluno} media: #{media} - Aprovado"
+    elsif media < 6 && media >= 3 
+     puts "aluno: #{aluno} media: #{media} - Recuperação!"
+    elsif media < 3 && media >= 0
+     puts"aluno: #{aluno} media: #{media} - Reprovado"
+    else
+      puts "Alguma nota está inválida"
+   end
+  end
 end
 
-escola = Escola.new
-
-escola.boletim(aluno, nota1, nota2, nota3)
+school = School.new(aluno, nota1, nota2, nota3)
+school.boletim
