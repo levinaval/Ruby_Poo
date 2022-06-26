ebc8e31960a0a7b3889634f7c92e80014edd8656
levi <levisantosluna@gmail.com>
2022-06-25 22:56:43 -0300

    Correção Ruby exercício

--- ruby_exercicios/atividades/multiplo.rb
+++ ruby_exercicios/atividades/multiplo.rb
@@ -1,21 +1,6 @@
 ##Escreva um programa que leia um número e mostre se ele é múltiplo de 7.
 
-#puts "Digite um número"
-#
-#numero = gets.chomp.to_i
-#
-#lista = numero * 7
-#
-#if lista <= 0
-#  puts "O resoltado = #{lista} sendos assim numero de 7"
-#else
-#  puts "O resoltado = #{lista} não sendos assim numero de 7"
-#end
-
-# or
-
 puts "Dígite um número."
-
 numero = gets.chomp.to_i
 
 resoltado = numero * 7
@@ -34,4 +19,4 @@ def decisao(resoltado)
   
 end
 
-decisao(resoltado)"""
+puts decisao(resoltado)
