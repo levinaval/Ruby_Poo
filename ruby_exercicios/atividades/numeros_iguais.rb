cc8958bbd2838ff3d104663b25bc93bb693e8366
levi <levisantosluna@gmail.com>
2022-06-25 23:13:01 -0300

    Números iguais Ruby exercício

--- ruby_exercicios/atividades/numeros_iguais.rb
+++ ruby_exercicios/atividades/numeros_iguais.rb
@@ -1,1 +1,22 @@
 #Escreva um programa que leia dois números e informe se eles são iguais.
+
+print "Digite o primeiro número:"
+number_one = gets.chomp.to_i
+
+print "Digite o segundo número:"
+number_two = gets.chomp.to_i
+
+def number_equal(number_one, number_two)
+  if number_one == number_two
+    puts
+    puts "São números iguais!"
+  elsif number_one > number_two
+    puts
+    puts "O primeiro número é maior!"
+  elsif number_two > number_one
+    puts
+    puts "O segundo número é o maior!"
+  end
+end
+
+puts number_equal(number_one, number_two)
