ee3d56d58b14c6e2733140b9bd45a9f4c60ebe63
levi <levisantosluna@gmail.com>
2022-06-25 23:24:33 -0300

    Números negativi ou positivo Ruby exercício

--- ruby_exercicios/atividades/numero_positivo.rb
+++ ruby_exercicios/atividades/numero_positivo.rb
@@ -1,1 +1,16 @@
 #Escreva um programa que leia um número e mostre se ele é positivo. 
+
+print "Escreva um número:"
+number = gets.chomp.to_i
+
+def number_positive(number)
+ if number > 0
+   puts "O número #{number} é positivo."
+ elsif number < 0
+  puts "O número #{number} é negativo."
+ else
+   puts "O número #{number} é zero!"
+ end
+end
+
+puts number_positive(number)
