b7e556ac15795fdb7ad5bf894f28010580dc4e1a
levi <levisantosluna@gmail.com>
2022-06-27 16:22:51 -0300

    Apredendo o attr_writer

--- assesores_de_atributos/attr_writer.rb
+++ assesores_de_atributos/attr_writer.rb
@@ -1,4 +1,10 @@
+# O attr_writer só permite que escreva os atributos, mas não possa lê-los.
+
 class Mascara
   attr_writer :m1, :m2
-  
 end
+
+mascara = Mascara.new
+
+mascara.m1 = "Batman"
+mascara.m2 = "Pantera Negra" 
