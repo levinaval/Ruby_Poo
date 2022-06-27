ee3e41b951e0776791cb65b1d834453d576c0739
levi <levisantosluna@gmail.com>
2022-06-26 21:54:41 -0300

    indentação

--- ruby_exercicios/atividades/multiplo.rb
+++ ruby_exercicios/atividades/multiplo.rb
@@ -6,17 +6,11 @@ number = gets.chomp.to_i
 resolt = number * 7
 
 def decision(resolt)
-
   if resolt <= 0
-
     puts "Resultado é #{resolt} Este não é múltiplo de 7."
-
   else
-
     puts "Resultado é #{resolt} Este é múltiplo de 7."
-
   end
-  
 end
 
 puts decision(resolt)
