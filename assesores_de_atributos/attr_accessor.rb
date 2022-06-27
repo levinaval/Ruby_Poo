418bc19f01c8a592f3f8591b639f87e872578515
levi <levisantosluna@gmail.com>
2022-06-27 10:31:21 -0300

    Aprendedo a fazer os attr_accessor, reader, writer

---
+++ assesores_de_atributos/attr_accessor.rb
@@ -0,0 +1,15 @@
+# o attr_accessor ja cria os métodos automaticamente para fazer funcionar toda a class.
+
+class Escola
+  attr_accessor :professor, :materia, :aluno
+end
+
+escola = Escola.new
+
+escola.professor = "professor: Marco Castro"
+escola.materia = "materia: TI"
+escola.aluno = "aluno: Lucas Amorim"
+
+puts escola.professor
+puts escola.materia
+puts escola.aluno
