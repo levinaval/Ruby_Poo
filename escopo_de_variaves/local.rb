dfbb90fa2bb2363fc9a154808f4595f3be07046f
levi <levisantosluna@gmail.com>
2022-06-28 23:39:50 -0300

    passando para o inglês local

--- escopo_de_variaves/local.rb
+++ escopo_de_variaves/local.rb
@@ -1,13 +1,13 @@
 # Declarada com a primeira letra de seu nome com uma letra minúscula ou sublinhado.
 # Só pode ser acessada apenas onde foi criada, dentro do método, classe, Etc. 
 
-def casa
-  local = "Esta casa é na rua Clovis Cavalcante."
-  print local
+def House
+  place = "Esta casa é na rua Clovis Cavalcante."
+  print place
 end
 
-casa
+House
 
 # vareavel de mesmo nome, porem com outro valor.
-casa = 1287
-puts casa
+House = 1287
+puts House
