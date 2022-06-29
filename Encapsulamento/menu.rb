ac5417bbfbdc54c6a9c3f01d053ef49fcf9037ac
levi <levisantosluna@gmail.com>
2022-06-29 19:35:40 -0300

    desenvolvendo o código menu

--- Encapsulamento/menu.rb
+++ Encapsulamento/menu.rb
@@ -33,11 +33,12 @@ menu_de_bebidas = MenuDeBebidas.new("Suco", "Refrigerante", "Água de cocô", "C
 
 puts "Olá! benvindo a Lanchonete Delícia"
 puts
-puts "Gostaria de fazer o seu pedido?"
+print "Gostaria de fazer o seu pedido? "
 decisao = gets.chomp
 
-def cliente(decisao)
+def cliente(decisao, menu_de_lanche)
  if decisao == "sim"
+   puts
    puts menu_de_lanche.menu1
  elsif decisao == "não"
    puts "Ok! Até a procima."
@@ -46,15 +47,15 @@ def cliente(decisao)
  end
 end
 
-puts cliente(decisao)
+puts cliente(decisao, menu_de_lanche)
 
 pedido_do_lanche = []
 pedido_do_lanche = gets.chomp
 
-puts "Quer pedir algo para beber?"
-decisao2 << gets.chomp
+print "Quer pedir algo para beber? "
+decisao2 = gets.chomp
 
-def cliente2(decisao2)
+def cliente2(decisao2, menu_de_bebidas)
   if decisao2 == "sim"
     puts menu_de_bebidas.menu2
   elsif decisao2 == "não"
@@ -64,17 +65,78 @@ def cliente2(decisao2)
   end  
 end
 
-puts cliente2(decisao2)
+puts cliente2(decisao2, menu_de_bebidas)
 
 pedido_do_bebida = []
 pedido_do_bebida = gets.chomp
 
-class LanchePedido
-  def initialize(pedido_do_lanche)
+class PedidoDoCliente
+  def initialize(pedido_do_lanche, pedido_do_bebida)
     @pedido_do_lanche = pedido_do_lanche
+    @pedido_do_bebida = pedido_do_bebida
   end
 
-  def mostra_pedido1
+  def mostrar_carrinho_de_pedidos
+    puts
+    puts "Pedido realizado:"
     puts "#{@pedido_do_lanche}"
+    puts "#{@pedido_do_bebida}"
   end
 end
+
+pedido = PedidoDoCliente.new(pedido_do_lanche, pedido_do_bebida)
+pedido.mostrar_carrinho_de_pedidos
+
+puts
+print "Algo mais? "
+decisao3 = gets.chomp
+
+def escolha(decisao3)
+ if decisao3 == "sim"
+   puts "Lanche ou bebida"
+ elsif decisao3 == "não"
+   puts "Pedido finalizando."
+   puts "É para entrega ou retirada na loja?"
+ else
+   puts "Não compreendemos a sua opção."
+ end
+end
+
+puts escolha(decisao3)
+ 
+decisao4 = gets.chomp
+#def destino_do_pedido(decisao5)
+# if decisao5 == "entrega"
+#   puts "Seu pedido chegará entre 40 a 60 minutos"
+# elsif decisao5 == "loja"
+#   puts "Seu pedido poderá ser retirado da LOJA entre 40 a 50 minutos!"
+# end
+#end
+#
+#puts destino_do_pedido(decisao5)
+
+
+def inclusão(decisao4, menu_de_lanche, menu_de_bebidas)
+ if decisao4 == "lanche"
+  puts menu_de_lanche.menu1
+ elsif decisao4 == "bebida"
+  puts menu_de_bebidas.menu2
+ end
+end
+novo_pedido = []
+novo_pedido = gets.chomp
+
+
+puts "Algo mais?"
+decisao6 = gets.chomp
+
+def inclusão2(decisao6, menu_de_lanche, menu_de_bebidas)
+  if decisao5 == "lanche"
+    puts menu_de_lanche.menu1
+  elsif decisao5 == "bebida"
+    puts menu_de_bebidas.menu2
+ end
+end
+
+novo_pedido2 = []
+novo_pedido2 = gets.chomp
