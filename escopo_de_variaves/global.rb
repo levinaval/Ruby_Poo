2b06281805051d6adc181b700f1307e8308aa735
levi <levisantosluna@gmail.com>
2022-07-03 23:01:18 -0300

    passando para o inglês

--- escopo_de_variaves/global.rb
+++ escopo_de_variaves/global.rb
@@ -2,14 +2,14 @@
 # Pode ser acessada em qualquer lugar do programa. OBS: "Fortemente desencorajado o seu uso."
 
 class Restalrante
-  def primeiro_pedido
+  def first_request
     $global = 0
     puts $global
   end
 end
 
 class Lanchonete
-  def segundo_pedido
+  def second_request
     $global += 1
     puts $global    
   end
@@ -18,8 +18,8 @@ end
 restalrante = Restalrante.new
 lanchonete = Lanchonete.new
 
-restalrante.primeiro_pedido
-lanchonete.segundo_pedido
-lanchonete.segundo_pedido
+restalrante.first_request
+lanchonete.second_request
+lanchonete.second_request
 # Por isso é muito perigosa! pode ser chamada por qualquer método, qualquer lugar pode altera-la. 
 puts $global
