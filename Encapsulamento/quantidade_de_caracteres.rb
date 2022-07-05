f9bc7805745bda59b3c653dca7232129cfb6d105
levi <levisantosluna@gmail.com>
2022-07-05 20:28:53 -0300

    Contegem de caracteres

---
+++ Encapsulamento/quantidade_de_caracteres.rb
@@ -0,0 +1,7 @@
+# Escreva um programa que leia dois nomes e mostre o que contém maior quantidade de caracteres.
+
+print "Escreva o primeiro nome: "
+name1 = gets.chomp
+
+print "Escreva o segundo nome: "
+name2 = gets.chomp
