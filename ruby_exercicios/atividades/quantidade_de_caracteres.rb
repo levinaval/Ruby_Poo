50193f80cc3e5568c4cd4e48bc2488d783316ca9
levi <levisantosluna@gmail.com>
2022-07-05 20:49:06 -0300

    Contegem de caracteres

--- ruby_exercicios/atividades/quantidade_de_caracteres.rb
+++ ruby_exercicios/atividades/quantidade_de_caracteres.rb
@@ -8,16 +8,24 @@ name2 = gets.chomp
 
 def contagem_de_caracteres(name1, name2)
   
-  puts name1.length 
-  puts name2.length 
+  name1.length 
+  name2.length 
 
 end
 
 contagem_de_caracteres(name1, name2)
 
-#def maior_quantidade(name1, name2)
-  
-  
-#end
+def maior_quantidade(name1, name2)
+  if name1.length > name2.length
+    puts
+    puts "O nome #{name1} é o que tem o maior números com #{name1.length} de caracteres!"
+  elsif name1.length < name2.length
+    puts
+    puts "O nome #{name2} é o que tem o maior números com #{name2.length} de caracteres!"
+  else
+    puts
+    puts "O nome #{name1} e o nome #{name2}, iguais com o mesmo número de caracteres com #{name1.length} cada!"
+  end    
+end
 
-#maior_quantidade(name1, name2)
+maior_quantidade(name1, name2)
