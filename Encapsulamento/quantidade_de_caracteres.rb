5bdc1bed083138dc42ddd98b89222b3279ccfa5a
levi <levisantosluna@gmail.com>
2022-07-05 20:38:39 -0300

    Contegem de caracteres Poo

--- Encapsulamento/quantidade_de_caracteres.rb
+++ Encapsulamento/quantidade_de_caracteres.rb
@@ -5,3 +5,27 @@ name1 = gets.chomp
 
 print "Escreva o segundo nome: "
 name2 = gets.chomp
+
+class Contagem
+  def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+  end
+end
+
+class MaisCaracteres
+  def caracteres(@name1, @name2)
+    @name1.length
+    @name2.length
+  end
+
+  def Contagem
+    if @name1 > @name2 
+      puts "Este nome #{@name1} tem #{@name1.length} caracteres!"
+    elsif 
+      
+    else
+      
+    end
+  end
+end
