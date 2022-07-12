2cf4a20775d645d3d2b088b34cbf412b44694bbe
levi <levisantosluna@gmail.com>
2022-07-12 06:29:49 -0300

    classe Sereais

--- Encapsulamento/.rb
+++ Encapsulamento/.rb
@@ -1,24 +1,27 @@
 class Sereais
-  attr_reader :quantidade, :produto, :valor, :desconto
-  def initialize(quantidade, produto, valor, desconto)
+  attr_reader :quantidade, :produto, :valor
+  def initialize(quantidade, produto, valor)
     @quantidade = quantidade
     @produto = produto
     @valor = calcular_desconto(valor)
-    @desconto = desconto
+    #@desconto = desconto
   end
 
   def comunicar
-    puts "O produto do tipo #{produto} custa #{valor}, tendo uma quantidade de #{quantidade}kl tera um descontonto de #{desconto}, ficando assim com um valor de #{valor}"
+    puts "O produto do tipo #{produto} custa #{valor}, tendo uma quantidade de #{quantidade}kl tera um descontonto de , ficando assim com um valor de #{valor}"
   end
   
   private
   def calcular_desconto(valor)
     if produto == 100
-     puts
-     elsif 
-     
+     valor * 1.9
+     elsif produto == 90
+     valor * 0.9
      else
-     
+     valor
     end  
   end
 end
+
+mercado = Sereais.new(100, "Feijão", 5)
+puts mercado.comunicar
