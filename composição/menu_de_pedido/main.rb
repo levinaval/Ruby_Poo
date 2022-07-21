7b31b777da3860f0538902d160b2f0635d4af8e1
levi <levisantosluna@gmail.com>
2022-07-20 23:12:00 -0300

    passando para o inglês main.rb

--- composição/menu_de_pedido/main.rb
+++ composição/menu_de_pedido/main.rb
@@ -1,58 +1,58 @@
-require_relative 'menu_lanches'
-require_relative 'menu_bebidas'
-require_relative 'forma_de_entrega'
+require_relative 'menu_lanche'
+require_relative 'menu_bebida'
+require_relative 'forma_de_entrga'
 
-menu_lanches = MenuLanches.new("Hamburger", "Cachorro Quente", "Batata Frita")
-menu_bebidas = MenuBebidas.new(["Suco", "Refrigerante"])
-forma_de_entrega = RetiradaDePedido.new("O pedido estará disponível para a retirada em 40 minutos!", "O pedido estará saindo para a entrega em 40 minutos!")
+snacks_menu = MenuLanches.new("Hamburger", "Cachorro Quente", "Batata Frita")
+drinks_menu = MenuBebidas.new(["Suco", "Refrigerante"])
+order_ithdrawal = RetiradaDePedido.new("O pedido estará disponível para a retirada em 40 minutos!", "O pedido estará saindo para a entrega em 40 minutos!")
 
 puts "-*-*-*-*-*"
 puts "Menu Lanches - Escolha o seu Lanche:"
 
-puts "1. #{menu_lanches.lanche1}"
-puts "2. #{menu_lanches.lanche2}"
-puts "3. #{menu_lanches.lanche3}"
+puts "1. #{snacks_menu.snacks1}"
+puts "2. #{snacks_menu.snacks2}"
+puts "3. #{snacks_menu.snacks3}"
 
 print "Escolha o seu lanche: "
-lanche = gets.chop.to_i
+snacks = gets.chop.to_i
 
-case lanche
+case snacks
 when 1
-  resultado = menu_lanches.lanche1
+  result = snacks_menu.snacks1
 when 2
-  resultado = menu_lanches.lanche2
+  result = snacks_menu.snacks2
 when 3
-  resultado = menu_lanches.lanche3
+  result = snacks_menu.snacks3
 end
 
 puts 
-puts resultado
+puts result
 
 puts "-*-*-*-*-*"
 puts "Menu bebida:"
-menu_bebidas.options.size.times do |posicao|
-  puts "#{posicao + 1} #{menu_bebidas.options[posicao]}"
+drinks_menu.options.size.times do |posicao|
+  puts "#{posicao + 1} #{drinks_menu.options[posicao]}"
 end
 
 print "Escolha a sua bebida: "
-bebida = gets.chop.to_i
+drinks = gets.chop.to_i
 puts
 
-case bebida
+case drinks
 when 1
-  puts menu_bebidas.options[1]
+  puts drinks_menu.options[1]
 when 2
-  puts menu_bebidas.options[0]
+  puts drinks_menu.options[0]
 end
 
 puts "É para entregar(1) ou retirar na loja(2)?"
-decisao = gets.chop.to_i
+decision = gets.chop.to_i
 
-case decisao
+case decision
 when 1
-puts forma_de_entrega.entrega  
+puts order_ithdrawal.delivery  
 when 2
-puts forma_de_entrega.retirada_na_loja  
+puts order_ithdrawal.pick_up_in_store  
 else
   puts "Opção inezistente!"
 end
