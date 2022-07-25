53fac33ff933830b66f227baf7834f48140b549d
levi <levisantosluna@gmail.com>
2022-07-25 16:21:21 -0300

    Atualizando

---
+++ Herança/class_aluno.rb
@@ -0,0 +1,3 @@
+#3. Cria uma classe Aluno com o atributo matrícula e série, Aluno herdará de Pessoa. Aluno terá um método chamado
+#apresentar (sobreescrita) que imprimirá a seguinte frase:
+#Olá! Eu sou Aluno #{nome} da série #{serie} com a seguinte matrícula #{matrícula}.
