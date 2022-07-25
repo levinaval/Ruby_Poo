acc853dc5d3b0dd5f2916a93c7d58fad45213b4c
levi <levisantosluna@gmail.com>
2022-07-25 15:26:17 -0300

    Herança class pessoa

--- Herança/atv_herança.2/class_pessoa.rb
+++ Herança/atv_herança.2/class_pessoa.rb
@@ -1,8 +1,6 @@
 #1. Criar uma classe Pessoa com atributo nome, idade. Pessoa terá um método chamado apresentar que imprimirá
 #a seguinte frase: Olá! Eu sou classe Pessoa.
 
-#2. Cria uma classe Professor com o atributo disciplina, Professor herdará de Pessoa. Professor terá um método chamado
-#apresentar (sobreescrita) que imprimirá a seguinte frase: Olá! Eu sou Professor #{nome} da disciplina #{disciplina}.
 
 #3. Cria uma classe Aluno com o atributo matrícula e série, Aluno herdará de Pessoa. Aluno terá um método chamado
 #apresentar (sobreescrita) que imprimirá a seguinte frase:
@@ -12,7 +10,12 @@
 #apresentar (sobreescrita) que imprimirá a seguinte frase: Olá! Eu sou Diretor #{nome} do setor #{setor}.
 
 class Pessoa
- def apresentar
+  attr_reader :name, :idade
+  def initialize(name, idade)
+    @name, @idade = name, idade
+  end
+  
+  def apresentar
    puts "Olá! Eu sou classe Pessoa."
- end
+  end
 end
