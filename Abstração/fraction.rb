2892e86e92e0ff06b71552f3f2790a17ef1c4db9
levi <levisantosluna@gmail.com>
2022-07-26 15:28:39 -0300

    Abstração fraction

--- Abstração/fraction.rb
+++ Abstração/fraction.rb
@@ -7,10 +7,13 @@
 
 # Fraction#to_s - Método que retorna um string com a fração
 
-# Fraction#* - Método que recebe outro objeto de Fração ou número inteiro e retorna uma novo
-#objeto do tipo Fraction com o resultado da multiplicação
+# Fraction#* - Método que recebe outro objeto de Fração ou número inteiro e retorna uma novo objeto do tipo Fraction com o resultado da multiplicação
 
 #Métodos de acesso às variáveis de numerador e denominador
+
+numerador = gets.chomp#.to_f
+puts
+denominador = gets.chomp#.to_f
 
 class Fraction
   def initialize(numerador, denominador)
@@ -18,6 +21,19 @@ class Fraction
   end
 
   def convercao
-    
+    numerador.to_f = @numerador
+    denominador.to_f = @denominador 
+  end
+
+  def comunicar
+    puts "A nossa fração é #{@numerador.to_f} / #{@denominador.to_f}"
+  end
+
+  def multiplicar
+   @numerador.to_f * @denominador.to_f
   end
 end
+
+d1 = Fraction.new(numerador, denominador)
+d1.comunicar
+d1.multiplicar
