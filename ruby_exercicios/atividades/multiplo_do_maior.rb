e67469376072f38c8ce7afacb6831d9949c6bdf8
levi <levisantosluna@gmail.com>
2022-08-03 22:07:57 -0300

    dois números maior e menor... se são multiplos

--- ruby_exercicios/atividades/multiplo_do_maior.rb
+++ ruby_exercicios/atividades/multiplo_do_maior.rb
@@ -1,2 +1,17 @@
 #  Escreva um programa que leia dois números e informe se o maior é múltiplo do menor. 
 
+print "Escolha um número entre 1 a 9: "
+primeiro = gets.chomp.to_f
+
+print "Escolha um número aparte de 10: "
+segundo = gets.chomp.to_f
+
+resultado = segundo / primeiro
+
+#puts resultado
+
+if resultado <= 0
+  puts "é múltiplo"
+elsif resultado <= 0.0
+  puts "não é múltiplo"  
+end
