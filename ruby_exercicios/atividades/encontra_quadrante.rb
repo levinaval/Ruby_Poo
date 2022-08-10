951d71899090b0a66bacf7a11bcd23ac4df74a44
levi <levisantosluna@gmail.com>
2022-08-09 23:03:13 -0300

    criando encontra quadrante

--- ruby_exercicios/atividades/encontra_quadrante.rb
+++ ruby_exercicios/atividades/encontra_quadrante.rb
@@ -1,1 +1,15 @@
 # Escreva um programa que leia um número inteiro que corresponde a um ângulo e informe em qual quadrante este ângulo se encontra. 
+
+prit "Digite um número para a reta X: "
+x = gets.chomp.to_i
+
+prit "Digite um número para a reta Y: "
+y = gets.chomp.to_i
+
+if x > 0 && y > 0
+  puts "Primeiro quadrante"
+elsif 
+  
+else
+  
+end
