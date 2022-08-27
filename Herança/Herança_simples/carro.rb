cf1dea39faafac02a645afd84c00a79d9921e0f8
levi <levisantosluna@gmail.com>
2022-08-27 20:14:11 -0300

    Herança simples carro

---
+++ Herança/Herança_simples/carro.rb
@@ -0,0 +1,26 @@
+class Veiculo
+  attr_reader :modelo
+
+  def initialize(modelo)
+    @modelo = modelo
+  end
+
+  def ligar_carro
+    puts "o carro esta ligado!"
+  end
+end
+
+class Carro < Veiculo
+  attr_reader :marca
+
+  def initialize(modelo, marca)
+  super(Veiculo)
+  @marca = marca
+  end
+end
+
+celta = Carro.new("celta", "chevrolet")
+
+puts celta.modelo
+puts celta.marca
+celta.ligar_carro
