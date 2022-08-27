b0ce57b82557645dfeafeeebf631c822a28f3283
levi <levisantosluna@gmail.com>
2022-08-27 20:12:39 -0300

    Herança simples criando cachorro

---
+++ Herança/Herança_simples/cachorro.rb
@@ -0,0 +1,30 @@
+class Animal
+  attr_reader :nome
+
+def initialize(nome)
+  @nome = nome
+end
+
+def comunicar
+ puts "classe mãe!"
+end
+end
+
+class Cachorro < Animal
+attr_reader :raca
+
+def initialize(nome, raca)
+ super(nome)
+ @raca = raca
+end
+
+def comunicar
+ puts "au, au, au = sou seu filho!"
+end
+end
+
+marlei = Cachorro.new("marlei", "pit bull")
+
+puts marlei.nome
+puts marlei.raca
+marlei.comunicar
