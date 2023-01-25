8e59f89f0f94560635d7426f690ad0631a94fc49
levi <levisantosluna@gmail.com>
2023-01-24 23:37:50 -0300

    Tradusindo empresa para o inglês

--- Herança/atv_herança/empresa.rb
+++ Herança/atv_herança/empresa.rb
@@ -1,28 +1,28 @@
-class Pessoa
-  attr_reader :nome, :cargo, :empresa
-  def initialize(nome, cargo, empresa)
-    @nome, @cargo, @empresa = nome, cargo, empresa
+class Person
+  attr_reader :name, :title, :company
+  def initialize(name, title, company)
+    @name, @title, @company = name, title, company
   end
 
-  def apresentação
-    puts "Eu sou #{@nome}, trabalho na #{@empresa} e já fui promovido para o cargo de #{@cargo}."
-  end
-end
-
-p = Pessoa.new("Lucas", "Diretor", "Petrobrans")
-p.apresentação
-
-class Empresa < Pessoa
-  attr_reader :nome, :cargo, :empresa
-  def initialize(nome, cargo, empresa, sede)
-    @nome, @cargo, @empresa, @sede = nome, cargo, empresa, sede
-  end
-
-  def nomeação
-    puts "Nós da empresa #{@empresa}, estamos aqui para parabenizar o colaborador #{@nome} pela sua promoção a #{@cargo} da nossa sede em #{@sede}."
+  def presentation
+    puts "Eu sou #{@name}, trabalho na #{@company} e já fui promovido para o cargo de #{@title}."
   end
 end
 
-e = Empresa.new("Lucas", "Diretor", "Petrobrans", "Rezende")
+p = Person.new("Lucas", "Diretor", "Petrobrans")
+p.presentation
 
-e.nomeação
+class Company < Person
+  attr_reader :sede
+  def initialize(name, title, company, sede)
+    @name, @title, @company, @sede = name, title, company, sede
+  end
+
+  def appointment
+    puts "Nós da empresa #{@company}, estamos aqui para parabenizar o colaborador #{@name} pela sua promoção a #{@title} da nossa sede em #{@sede}."
+  end
+end
+
+e = Company.new("Lucas", "Diretor", "Petrobrans", "Rezende")
+
+e.appointment
