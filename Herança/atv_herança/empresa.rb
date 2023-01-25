d0fee98f9b9754d6a4756a7638b58a79c5758da9
levi <levisantosluna@gmail.com>
2023-01-24 23:05:02 -0300

    Criando class empresa na categoria herança

---
+++ Herança/atv_herança/empresa.rb
@@ -0,0 +1,14 @@
+class Pessoa
+  attr_reader :cargo, :empresa
+  def initialize(nome, cargo, empresa)
+    @nome, @cargo, @empresa = nome, cargo, empresa
+  end
+
+  def apresentação
+    puts "Eu sou #{@nome}, trabalho na #{@empresa} e já fui promovido para o cargo de #{@cargo}."
+  end
+end
+
+#p = Pessoa.new("Lucas", "Diretor", "Petrobrans")
+#p.apresentação
+
