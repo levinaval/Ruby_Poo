21afc62c1bc452461e1ad6b66c62f266b4df631c
levi <levisantosluna@gmail.com>
2023-01-25 22:46:04 -0300

    Ultima correção em empresa

--- Herança/atv_herança/empresa.rb
+++ Herança/atv_herança/empresa.rb
@@ -12,15 +12,17 @@ class Company < Person
     @sede = sede
   end
 
-  #def appointment
-  #  puts "Nós da empresa #{@company}, estamos aqui para parabenizar o colaborador #{@name} pela sua promoção a #{@title} da nossa sede em #{@sede}."
-  #end
+  def appointment
+    puts "Nós da empresa #{@company}, estamos aqui para parabenizar o colaborador #{@name} pela sua promoção a #{@title} da nossa sede em #{@sede}."
+  end
 end
 
 e = Company.new("Lucas", "Diretor", "Petrobrans", "Rezende")
 
 #e.appointment
-puts e.name
-puts e.title
-puts e.company
-puts e.sede
+#puts e.name
+#puts e.title
+#puts e.company
+#puts e.sede
+
+e.appointment
