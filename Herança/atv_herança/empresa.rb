3dd67c929e1f68965bef99570e419b547fafba88
levi <levisantosluna@gmail.com>
2023-01-25 22:43:54 -0300

    Concluindo correções em empresa

--- Herança/atv_herança/empresa.rb
+++ Herança/atv_herança/empresa.rb
@@ -8,14 +8,19 @@ end
 class Company < Person
   attr_reader :sede
   def initialize(name, title, company, sede)
-    @name, @title, @company, @sede = name, title, company, sede
+    super(name, title, company)
+    @sede = sede
   end
 
-  def appointment
-    puts "Nós da empresa #{@company}, estamos aqui para parabenizar o colaborador #{@name} pela sua promoção a #{@title} da nossa sede em #{@sede}."
-  end
+  #def appointment
+  #  puts "Nós da empresa #{@company}, estamos aqui para parabenizar o colaborador #{@name} pela sua promoção a #{@title} da nossa sede em #{@sede}."
+  #end
 end
 
 e = Company.new("Lucas", "Diretor", "Petrobrans", "Rezende")
 
-e.appointment
+#e.appointment
+puts e.name
+puts e.title
+puts e.company
+puts e.sede
