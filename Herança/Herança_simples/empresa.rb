3906d667383cf6ab5360b1b05d1ab29141e9eab6
levi <levisantosluna@gmail.com>
2023-01-25 23:04:20 -0300

    Criando número maior

---
+++ Herança/Herança_simples/empresa.rb
@@ -0,0 +1,28 @@
+class Person
+  attr_reader :name, :title, :company
+  def initialize(name, title, company)
+    @name, @title, @company = name, title, company
+  end
+end
+
+class Company < Person
+  attr_reader :sede
+  def initialize(name, title, company, sede)
+    super(name, title, company)
+    @sede = sede
+  end
+
+  def appointment
+    puts "Nós da empresa #{@company}, estamos aqui para parabenizar o colaborador #{@name} pela sua promoção a #{@title} da nossa sede em #{@sede}."
+  end
+end
+
+e = Company.new("Lucas", "Diretor", "Petrobrans", "Rezende")
+
+#e.appointment
+#puts e.name
+#puts e.title
+#puts e.company
+#puts e.sede
+
+e.appointment
